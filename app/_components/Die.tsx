33678fb466b3296ddaef0c14fd5a1090db110be7
Dom-iha <taqib.ibrahim@gmail.com>
2024-04-09 22:42:32 +0100

    balooon game structure

--- app/_components/Die.tsx
+++ app/_components/Die.tsx
@@ -4,13 +4,13 @@ interface DieProps {
   hold: () => void;
 }
 
-const Die:React.FC = (props: DieProps) => {
+const Die = (props: DieProps) => {
   const variant = props.held ? 'bg-[#59E391]' : 'bg-[white]';
 
   return (
     <button
       type='button'
-      className={`die-face focus-visible:outline-dashed outline-offset-2 focus-visible:outline-1 focus-visible:outline-[#5035FF] rounded-lg w-[50px] h-[50px] text-[#2b283a] flex items-center justify-center text-base ${variant}`}
+      className={`die-face focus-visible:outline-dashed outline-offset-2 focus-visible:outline-1 focus-visible:outline-[#5035FF] rounded-lg w-[3.125rem] h-[3.125rem] text-[#2b283a] flex items-center justify-center text-base ${variant}`}
       onClick={props.hold}
     >
       <span className='text-3xl font-medium'>{props.value}</span>
