a735954a82fac5ac6d382f15b73b6daadd5a1c28
unknown <daudakolo16@gmail.com>
2024-04-16 12:41:53 +0100

    chore/component: Pre-commit

---
+++ app/learning/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SubjectCard from "../_components/SubjectCard";
+
+const Page = () => {
+  return (
+    <div className="w-full h-full flex flex-col">
+      <SubjectCard />
+    </div>
+  );
+};
+
+export default Page;
